a0205e14097e6aa5719d5abe3e901cad12613417
erdincozsertel <45659275+erdincozsertel@users.noreply.github.com>
2024-04-06 01:19:24 +0300

    Update filterXML.js
    Add entry element to output XML.
    Update generateFilterEntry fuction.
    Add loop to get all filiters.

--- gmail-filter/src/utils/filterXML.js
+++ gmail-filter/src/utils/filterXML.js
@@ -1,19 +1,55 @@
-export const baseXML = `<?xml version= "1.0 " encoding= "UTF-8 "?>
-<feed  xmlns= "http://www.w3.org/2005/Atom " xmlns:apps= "http://schemas.google.com/apps/2006 ">
-    <title>Mail Filiters</title>
-</feed>`;
+import { Category } from "./properties";
+export function generateOutputXML(filiters) {
+  let entryXML = generateFilterEntry(filiters);
+  return `<?xml version= "1.0 " encoding= "UTF-8 "?>
+    <feed  xmlns= "http://www.w3.org/2005/Atom " xmlns:apps= "http://schemas.google.com/apps/2006 ">
+        <title>Mail Filiters</title>
+        ${entryXML}
+    </feed>`;
+}
 
-export function generateFilterEntry(params) {
-    let entryXML = `
-      <entry>
-        <category term="filter">
-        </category>
-        <title>${params.title}</title>
-        ${properties}
-      </entry>`;
-      let properties = "";
-      for (const [key, value] of Object.entries(params.properties)) {
-        properties += `\n      <apps:property name="${key}" value="${value}"/>`;
+function generateFilterEntry(filiters) {
+  let entries = ``;
+  for (const [key, value] of Object.entries(filiters)) {
+    let properties = ``;
+    let params = filiters.params;
+    let hasAction = false;
+    let shouldTrash = false;
+
+    for (const [key, value] of Object.entries(params.properties)) {
+      if (key.section == "Action" && key.name != "shouldTrash") {
+        hasAction = true;
       }
-      return entryXML;
+      if (key.name != "shouldTrash") {
+        if (key.name != "smartLabelToApply") {
+          properties += `\n      <apps:property name="${key}" value="${value}"/>`;
+        } else {
+          //TODO: Convert to TypeScript to add smartlabel
+          // properties += `\n      <apps:property name="${key}" value="smartlabel_${Category[value as keyof typeof Category]}"/>`;
+        }
+      } else if (value == true) {
+        shouldTrash = true;
+      }
+    }
+    if (shouldTrash == true) {
+      if (hasAction == false) {
+        properties = `\n      <apps:property name="shouldTrash" value="true"/>`;
+      } else {
+        console.log("shouldTrash is true but hasAction is also true");
+      }
+    }
+
+    let entryXML = `
+        <entry>
+          <category term="filter">
+          </category>
+          <title>${filiters.title}</title>
+          ${properties}
+        </entry>`;
+    if (properties.length === 0) {
+      entryXML = ``;
+    }
+    entries += entryXML;
   }
+  return entries;
+}
